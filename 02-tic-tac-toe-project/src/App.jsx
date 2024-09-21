be08dce3248e4aa302088fecd9026e650c82c240
murtaz <murtazasbazaz@gmail.com>
2024-09-21 08:55:06 +0530

    started 2

--- 02-tic-tac-toe-project/src/App.jsx
+++ 02-tic-tac-toe-project/src/App.jsx
@@ -1,24 +1,30 @@
 
 function App() {
-  
+
 
   return (
-  <main>
-    <div id="game-container">
-     <ol id="players">
-      <li>
-        <span className="player-name">Player 1</span>
-        <span className="player-symbol">X</span>
-      </li>
-      <li>
-      <span className="player-name">Player 2</span>
-      <span className="player-symbol">O</span>
-      </li>
-     </ol>
-     Game Board
-    </div>
-    LOG
-  </main>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      LOG
+    </main>
   )
 }
 
